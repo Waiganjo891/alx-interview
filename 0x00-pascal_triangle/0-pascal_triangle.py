5561918df22344454a17067e918557ed08497100
Your Name <you@example.com>
2024-06-27 01:02:02 -0700

    Tasks

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This code will generate Pascal's Triangle up to the n-th row,
+ensuring the correct structure and values as demonstrated in the test script.
+"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
+
+
+if __name__ == "__main__":
+    def print_triangle(triangle):
+        for row in triangle:
+            print("[{}]".format(",".join([str(x) for x in row])))
+
+    print_triangle(pascal_triangle(5))
